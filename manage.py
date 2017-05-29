836d9903fcf504772e7f7258dbe3d1dc7e354159
David Martin <dmartin35@users.noreply.github.com>
2017-05-29 11:00:30 +0200

    make manage run with either dev or default
    in production, dev settings is not available, hence force it using
    default (ie. prod) settings - to simplify the use of manage.py without
    export DJANGO_SETTINGS_MODULE each time

--- manage.py
+++ manage.py
@@ -3,7 +3,9 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pronosfoot.settings.dev")
+    dev_found = os.path.exists(os.path.join(os.path.dirname(__file__), 'pronosfoot', 'settings', 'dev.py'))
+    settings_mod = "pronosfoot.settings.dev" if dev_found else "pronosfoot.settings"
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_mod)
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
