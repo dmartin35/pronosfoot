add6ff0e74ef419cfcc58045aad02bf8fdeb47ab
David Martin <dmartin35@users.noreply.github.com>
2020-08-25 22:36:12 +0200

    Allow re-schedule game in the past & ensure no date change when game has a score

--- admin/daily/calendar.py
+++ admin/daily/calendar.py
@@ -18,6 +18,7 @@ def check_calendar():
     if any change, set new date&time and log it
     """
     lfp_cal = get_calendar()
+    allow_schedule_backward = len(lfp_cal) == 380
     for fixt_dict in lfp_cal:
         #locally store fixture info
         team_a = Team.objects.get(name=fixt_dict['team_a'])
@@ -30,6 +31,11 @@ def check_calendar():
 
         #check date/time, modify if needed and log change
         if fixture:
+
+            # do not change a date for a match that has a final score
+            if fixture.score_a is not None or fixture.score_b is not None:
+                continue
+
             old_day = fixture.day
             old_time = fixture.hour
 
@@ -39,9 +45,12 @@ def check_calendar():
             # case 1: always update for a more recent day ie postponed/re-scheduled matches
             # case 2: same day but scheduled time has changed
             # case 3: was not scheduled, but possibly day(s) before
+            # case 4: game is reschedule before initial date in replacement of another TV game that is postponed
+            # (only when no duplicates!)
             if day > old_day \
                     or (day == old_day and old_time != hour) \
-                    or (day != old_day and str(old_time)=='00:00:00'):
+                    or (day != old_day and str(old_time) == '00:00:00') \
+                    or (day < old_day and allow_schedule_backward):
                 log_str = '%s (%s) %s-%s %s %s'% (fixture.id, fixture.week,
                     fixture.team_a, fixture.team_b, fixture.day, fixture.hour)
 
