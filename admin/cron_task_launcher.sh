7cd410db7609cbd993323ba7d87319d02e5fdb73
David Martin <dmartin35@users.noreply.github.com>
2022-07-07 09:27:26 +0200

    removes old admin script - now moved as django commands

--- admin/cron_task_launcher.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ $# -eq 0 ] ; then
-echo "Usage: $0 <django-admin command>"
-exit 1
-fi
-
-export DJANGO_SETTINGS_MODULE=pronosfoot.settings.prod
-/home/pronosfoot/.python/venv/pronosfoot/bin/python /home/pronosfoot/pronosfoot/manage.py ${*}
--- admin/daily_remind.py
+++
@@ -1,8 +0,0 @@
-# """
-# daily administration
-# 1.check forecasts and send reminder mail
-# """
-# from admin.daily.reminder_txt import check_forecasts
-#
-# if __name__ == '__main__':
-#     check_forecasts()
