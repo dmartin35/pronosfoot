91cd5150140bfa2c53849834855e303a62d7e2ba
David Martin <dmartin35@users.noreply.github.com>
2022-10-03 22:48:52 +0200

    fixes creating datetime with correct timezone (with daylight saving time changes)

--- tools/ical.py
+++ tools/ical.py
@@ -1,5 +1,6 @@
 from ics import Calendar, Event
 from datetime import timedelta, datetime
+import pytz
 
 
 def fixtures_to_ical(fixtures) -> str:
@@ -12,8 +13,8 @@ def fixtures_to_ical(fixtures) -> str:
         e.name = f"{f.team_a} - {f.team_b} (J{f.week}) [football]"
         e.uid = f"LFP_L1_{year_start}_{year_end}_{f.id}"
 
-        dt = f"{f.day}T{f.hour}+02:00"
-        e.begin = datetime.fromisoformat(dt)
+        dt = datetime.combine(f.day, f.hour).astimezone(pytz.timezone("Europe/Paris"))
+        e.begin = dt
         e.duration = timedelta(hours=2)
         #e.end = e.begin + timedelta(hours=2)
 
